bc204ddbfcc55fd7880be7001491571a684f2c85
Mohamed Farahat <farahats9@yahoo.com>
2021-09-17 22:18:48 +0200

    Initial commit

---
+++ _nuxt/f57e689.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{682:function(n,e,t){"use strict";t.r(e);var l={},c=t(14),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("n-child")}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1631909857/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631909857",layout:"default",error:a,serverRendered:true,routePath:b,config:{clientVersion:"1.0.0",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
